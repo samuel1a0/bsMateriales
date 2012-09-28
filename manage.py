23edc766bc6f316240fa719b3f740f53628d1900
emacer <emacer2011.gmail.com>
2012-09-28 15:16:00 -0300

    manage.py modificado

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsMateriales.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsMaterials.settings")
 
     from django.core.management import execute_from_command_line
 
